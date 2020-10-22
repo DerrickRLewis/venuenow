98d13d1b2bda190cad771c5b9709d700e71b32cb
Derrick <derrickrlewis@gmail.com>
2020-10-22 02:56:13 -0500

    Initial commit

---
+++ src/main/java/io/thefigure8/venuenow/VenuenowApplication.java
@@ -0,0 +1,13 @@
+package io.thefigure8.venuenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenuenowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VenuenowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/thefigure8/venuenow/VenuenowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.thefigure8.venuenow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenuenowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
